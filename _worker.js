351353387f28ad0fa790ccdc9b81d979382338cb
zhaoloveming <115675812+zhaoloveming@users.noreply.github.com>
2024-07-25 10:40:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '7c0ac091-c5d3-4b63-8a41-6bbd51aef29b';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
