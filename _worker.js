3ebae9553e1a6a649fd6b6d015210030db7153e2
zhaoloveming <115675812+zhaoloveming@users.noreply.github.com>
2024-07-25 10:50:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -129,7 +129,7 @@ export default {
 
                         let secretKey = await env.bpb.get('secretKey');
                         const pwd = await env.bpb.get('pwd');
-                        if (!pwd) await env.bpb.put('pwd', 'admin');
+                        if (!pwd) await env.bpb.put('pwd', 'mszhao');
 
                         if (!secretKey) {
                             secretKey = generateSecretKey();
