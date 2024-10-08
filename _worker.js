a501b7436eea8f97c93063d4d49862a8a045493d
zhaoloveming <115675812+zhaoloveming@users.noreply.github.com>
2024-10-08 16:44:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'f367e32c-4dce-45e1-ad4e-19519cfbb3d3';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com','158.101.148.105','129.150.50.63','144.24.83.39','152.70.90.168','140.83.86.166','47.243.118.63'];
